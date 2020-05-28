ee8acda9d859abf6f0618e3145b495e12b2f8b00
mankiran kaur <rishabhveersinghkailay@gmail.com>
2020-05-29 00:11:33 +0530

    changed contrast logic and added saturation control

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,7 @@ class Main {
 		window.addEventListener('load', (e) => {
 			brightness_input.onchange = this.onChange;
 			contrast_input.onchange = this.onChange;
+			saturation_input.onchange = this.onChange;
 			image_input.addEventListener('change', (e) => {
 				// console.log(e, e.target.result);
 				this.imageFile = e.target.files[0];
@@ -37,11 +38,11 @@ class Main {
 
 
 	applyChangesToCanvas = (change) => {
-		console.log('apply changes to canvas', this.changes);
 		let imageData = new ImageData(new Uint8ClampedArray(this.originalImageData.data), this.originalImageData.width);
 		let data = imageData.data;
 		let brightnessValue = parseInt(this.changes.brightness) || 0;
 		let contrastValue = parseInt(this.changes.contrast) || 0;
+		let saturationValue = parseInt(this.changes.saturation) || 0;
 
 		for(let i=0; i<data.length; i+=4) {
 			// brightness
@@ -53,16 +54,28 @@ class Main {
 			let avg = (data[i]+data[i+1]+data[i+2])/3;
 			if(avg > 122.5) {
 				// bright pixel
-				data[i] += contrastValue;
-				data[i+1] += contrastValue;
-				data[i+2] += contrastValue;
+				// * data[i]/avg canbe anything like data[i]/100 or data[i]/255 just need to be dependent on current rgb value (to make change in rgb value dependent on current rgb value) (else for two bright pixels it will increase brightness equally so it will not increase contrast in between two bright pixels)
+				data[i] += (contrastValue*(data[i]/avg));
+				data[i+1] += (contrastValue*(data[i+1]/avg));
+				data[i+2] += (contrastValue*(data[i+2]/avg));
 			}
 			else {
 				// dark pixel
-				data[i] -= contrastValue;
-				data[i+1] -= contrastValue;
-				data[i+2] -= contrastValue;	
+				data[i] -= (contrastValue*(data[i]/avg));
+				data[i+1] -= (contrastValue*(data[i+1]/avg));
+				data[i+2] -= (contrastValue*(data[i+2]/avg));
 			}
+
+			// saturation (increase the value of color which is more lol)
+			// at -100 saturation image should be b/w i.e. rgb = avg,
+		 	// if saturation is decreasing the gap between r, g, b values and avg should be decreasing, if saturation is -50 gap now should be half as it was before
+		 	// if saturation is increasing the gap betweem r, g, b values and avg should be increasing
+		 	// using saturation as percentage of diff increased or decreased as compared to original difference 
+			avg = (data[i]+data[i+1]+data[i+2])/3;
+			data[i] += (((data[i]-avg)/100)*saturationValue)
+			data[i+1] += (((data[i+1]-avg)/100)*saturationValue)
+			data[i+2] += (((data[i+2]-avg)/100)*saturationValue)
+
 		}
 		this.ctx.putImageData(imageData, 0, 0);
 	}
