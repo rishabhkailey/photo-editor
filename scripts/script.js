1997d35396d0f4ce1cdcffcb28bd00b80313d703
Rishabhveer singh <44227973+rishabhkailey@users.noreply.github.com>
2020-06-06 16:48:02 +0530

    change structure of the code.
    created different class for crop, imageAdjustment and rotate.

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,35 @@
-class Main {
-	fileReader = null;
-	imageFile = null;
-	image = null;
-	editedImage = null; // used by rotation (so it don't have to iterate over every pixel)
-	ctx = null;
-	aspectRatio = null;
-	screenAspectRatio = null;
-	originalImageData = null;
-	changes = {};
-	imageLoaded = false;
-	prevChangeName = null;
-	cropRect = null;
-	cropRectBoundingRect = null;
-	cropRectTranslate = null;
-	cropRectChangeType = null;
+"use strict"
 
-	constructor() { 
-		this.fileReader = new FileReader();
-		this.image = new Image();
-		this.editedImage = new Image();
+class Crop {
+	
+	constructor({setImageDataSrc}) {
+		this.setImageDataSrc = setImageDataSrc;
+		this.ctx = null;
+		this.cropRect = null;
+		this.canvasContainerBoundingRect = null;
+		this.cropRectBoundingRect = null;
 
-		// eventListeners
-        this.fileReader.onload = this.handleFileReader
-		// first time
-		this.image.onload = () => {
-			this.editedImage.src = this.image.src;
-		};
-		// after editing
-		this.editedImage.onload = this.reRenderCanvas;
+		// used to avoid checking for change type (changeType is same for one session(onMouseDown to onMouseUp))
+		this.cropRectChangeType = null;
 
-		window.addEventListener('resize', this.reRenderCanvas);
-		window.addEventListener('load', (e) => {
-			brightness_input.onchange = this.onChange;
-			contrast_input.onchange = this.onChange;
-			saturation_input.onchange = this.onChange;
-			bottom_option_input.onchange = this.onChange;
-			crop_button.onclick = this.onCropClick;
-			rotate_button.onclick = () => {this.bottomOption('rotate')}
-			image_input.addEventListener('change', (e) => {
-				// console.log(e, e.target.result);
-				this.imageFile = e.target.files[0];
-				this.fileReader.readAsDataURL(e.target.files[0]);
-			})
-		})
+		this.cropRectTranslate = {x: 0, y: 0, scaleX: 1, scaleY: 1};
+		this.prevMousePosition = null;
+		this.width = null;
+		this.height = null;
 	}
 
-	// crop
+	
+	resetConfirmButton() {
+		bottom_confirm_button.style.display = 'none';
+		bottom_confirm_button.name = null;
+	}
+
+	showConfirmButton(name) {
+		bottom_confirm_button.style.display = 'block';
+		bottom_confirm_button.name = name;
+		bottom_confirm_button.onclick = this.confirmCrop;
+	}
+
 	showCropRect= ()=> {
 		if(this.cropRect) {
 			return;
@@ -68,7 +52,7 @@ class Main {
 	hideCropRect = () => {
 		canvas_container.removeChild(this.cropRect);
 		this.cropRect = null;
-		this.cropRect.translate = null;
+		this.cropRectTranslate = null;
 		this.height = null;
 		this.width = null;
 		this.prevMousePosition = null;
@@ -107,10 +91,6 @@ class Main {
 		
 		this.cropRect.style.transform = `translate(${x}px, ${y}px) scaleX(${this.cropRectTranslate.scaleX}) scaleY(${this.cropRectTranslate.scaleY})`;
 	}
-
-	prevMousePosition = null;
-	width = null;
-	height = null;
 	changeCropRectDimensions(e) {
 		if(!this.prevMousePosition) {
 			this.prevMousePosition = {x: e.clientX, y: e.clientY};
@@ -200,31 +180,164 @@ class Main {
 	onCropClick = () => {
 		console.log(this.showCropRect);
 		this.showCropRect();
-
-		// this.cropRect.addEventListener('mousedown', this.startTrackingMouse);
-		// this.cropRect.addEventListener('mouseup', this.stopTrackingMouse);
 		this.cropRect.addEventListener('mousedown', this.startTrackingMouse);
 		window.addEventListener('mouseup', this.stopTrackingMouse);
-		
 	}
 
 	confirmCrop = () => {
+		let ctx = canvas.getContext('2d');
 		let {left, top, width, height} = this.cropRect.getBoundingClientRect();
 		let canvasRect = canvas.getBoundingClientRect();
-		let imageData = this.ctx.getImageData(left-canvasRect.left, top-canvasRect.top, width, height);
-		console.log(left-canvasRect.left, top-canvasRect.top, width, height, imageData);
-		this.ctx.clearRect(0, 0, canvas.width, canvas.height);
-		// let scaleX = canvas.width/width;
-		// let scaleY = canvas.height/height;
-		// console.log(scaleX, scaleY);
-		// this.ctx.scale(scaleX, scaleY);
+		let imageData = ctx.getImageData(left-canvasRect.left, top-canvasRect.top, width, height);
 
-		this.editedImage.src = getImageFromImageData(imageData, width, height);
-		console.log(this.editedImage);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		// this.ctx.putImageData(imageData, 0, 0);
+		this.setImageDataSrc(getImageFromImageData(imageData, width, height));
 		this.hideCropRect();
 		this.resetConfirmButton();
+	}
+
+}
+
+class ImageAdjustments {
+	constructor({getImageData, setImageData}) {
+		this.getImageData = getImageData;
+		this.setImageData = setImageData;
+	}
+	changeBrightNess(data, i, brightnessValue) {		
+		// brightness
+		data[i] += brightnessValue;
+		data[i+1] += brightnessValue;
+		data[i+2] += brightnessValue;
+	}
+	changeSatruration(data, i, saturationValue) {
+		// saturation (increase the value of color which is more lol)
+		// at -100 saturation image should be b/w i.e. rgb = avg,
+		// if saturation is decreasing the gap between r, g, b values and avg should be decreasing, if saturation is -50 gap now should be half as it was before
+		// if saturation is increasing the gap betweem r, g, b values and avg should be increasing
+		// using saturation as percentage of diff increased or decreased as compared to original difference 
+		let avg = (data[i]+data[i+1]+data[i+2])/3;
+		data[i] += (((data[i]-avg)/100)*saturationValue);
+		data[i+1] += (((data[i+1]-avg)/100)*saturationValue);
+		data[i+2] += (((data[i+2]-avg)/100)*saturationValue);
+
+	}
+	changeContrast(data, i, contrastValue) {
+		let avg = (data[i]+data[i+1]+data[i+2])/3;
+		// bright pixel
+		// equaly increase else color will change
+		// bright to more bright, further for two bright pixels increase in brightness of less bright pixel should be less than that of more bright pixel, so it can create contrast even in between two bright pixels
+		// to do so the value increased or decreased should depends on brightness of pixel
+		let value = contrastValue*((avg-122.5)/100)
+		data[i] += value;
+		data[i+1] += value;
+		data[i+2] += value;
+	}
+	applyChangesToCanvas = (changes) => {
+
+		let imageData = new ImageData(new Uint8ClampedArray(this.getImageData().data), this.getImageData().width);
+		let data = imageData.data;
+		let brightnessValue = parseInt(changes.brightness) || 0;
+		let contrastValue = parseInt(changes.contrast) || 0;
+		let saturationValue = parseInt(changes.saturation) || 0;
+		canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+		for(let i=0; i<data.length; i+=4) {
+			this.changeBrightNess(data, i, brightnessValue);
+			this.changeContrast(data, i, contrastValue);
+			this.changeSatruration(data, i, saturationValue);
+		}
+
+		canvas.getContext('2d').putImageData(imageData, 0, 0);
+	}
+}
+
+class Rotate {
+	
+	constructor({getImageData, setImageData, getImage}) {
+		this.getImageData = getImageData;
+		this.setImageData = setImageData;
+		this.getImage = getImage;
+		this.ctx = null;
+	}
+
+	initializeCtx(ctx) {
+		this.ctx = ctx;
+	}
+
+	rotateImage(changes) {
+		this.ctx = canvas.getContext('2d');
+		let rotateValue = parseInt(changes.rotate) || 0;
+		this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+		this.ctx.save();
+		this.ctx.translate(canvas.width/2, canvas.height/2);
+		this.ctx.rotate((Math.PI / 180) * rotateValue);
+		this.ctx.drawImage(this.getImage(), -canvas.width/2, -canvas.height/2, canvas.width, canvas.height);
+		this.ctx.restore();
+
+
+		// changing the imageData because putImageData used in applyChangesToCanvas(for adjustment) doesnot depends on rotaion of ctx (its an array for whole canvas, we need rotated arry for it to show rotated image), now we are changing the orginalImageData (array) to rotated image array 
+		let imageData = this.ctx.getImageData(0, 0, canvas.width, canvas.height);
+		// above we just rotate the canvas, now get the image data of rotated canvas and save the data
+		this.setImageData(new ImageData(new Uint8ClampedArray(imageData.data), imageData.width));
+	}
+}
+
+class Main {
+	imageFile = null;
+	imageData = null;
+	imageLoaded = false;
+	prevChangeName = null;
+
+	constructor() {
+		this.changes = {};
+		this.fileReader = new FileReader();
+		this.image = new Image();
+		this.editedImage = new Image();
+		this.ctx = null;
+		this.cropObject = new Crop({setImageDataSrc: this.setImage});
+		this.imageAdjustmentObject = new ImageAdjustments({setImageData: this.setImageData, getImageData: this.getImageData});
+		this.rotateObject = new Rotate({setImageData: this.setImageData, getImageData: this.getImageData, getImage: this.getImage});
+		// eventListeners
+        this.fileReader.onload = this.handleFileReader
+		// first time
+		this.image.onload = () => {
+			this.editedImage.src = this.image.src;
+		};
+		// first time and after editing
+		this.editedImage.onload = this.reRenderCanvas;
+
+		window.addEventListener('resize', this.reRenderCanvas);
+		window.addEventListener('load', (e) => {
+			brightness_input.onchange = this.onChange;
+			contrast_input.onchange = this.onChange;
+			saturation_input.onchange = this.onChange;
+			bottom_option_input.onchange = this.onChange;
+			crop_button.onclick = this.cropObject.onCropClick;
+			rotate_button.onclick = () => {this.bottomOption('rotate')}
+			image_input.addEventListener('change', (e) => {
+				// console.log(e, e.target.result);
+				this.imageFile = e.target.files[0];
+				this.fileReader.readAsDataURL(e.target.files[0]);
+			})
+		})
+	}
+
+	setImage = (src)=> {
+		this.editedImage.src = src; // data:base64 image/png .......
+	}
+	
+	getImage = ()=> {
+		return this.editedImage;
+	}
+
+	getImageData = () => {
+		return this.imageData;
+	}
+
+	setImageData = (data) => {
+		// this new data's reference is different(create by " new ImageData(data, width) ") ,referene is changed
+		this.imageData = data;
 	}
 
 	bottomOption(button) {
@@ -255,79 +368,12 @@ class Main {
 					this.editedImage.src = canvas.toDataURL('image/png');
 				}
 				this.prevChangeName = 'rotate';
-				this.rotateImage();
+				this.rotateObject.rotateImage(this.changes);
 			}
 			else {
 				this.prevChangeName = 'adjustment';
-				this.applyChangesToCanvas();
+				this.imageAdjustmentObject.applyChangesToCanvas(this.changes);
 			}
-	}
-
-	rotateImage() {
-		let rotateValue = parseInt(this.changes.rotate) || 0;
-		this.ctx.clearRect(0, 0, canvas.width, canvas.height);
-		this.ctx.save();
-		this.ctx.translate(canvas.width/2, canvas.height/2);
-		this.ctx.rotate((Math.PI / 180) * rotateValue);
-		this.ctx.drawImage(this.editedImage, -canvas.width/2, -canvas.height/2, canvas.width, canvas.height);
-		this.ctx.restore();
-
-
-		// changing the imageData because putImageData used in applyChangesToCanvas(for adjustment) doesnot depends on rotaion of ctx (its an array for whole canvas, we need rotated arry for it to show rotated image), now we are changing the orginalImageData (array) to rotated image array 
-		let imageData = this.ctx.getImageData(0, 0, canvas.width, canvas.height);
-		this.originalImageData = new ImageData(new Uint8ClampedArray(imageData.data), imageData.width);
-		// to apply rest of the updates like brightness, contrast, saturation
-		// this.applyChangesToCanvas();
-	}
-
-	applyChangesToCanvas = () => {
-		let imageData = new ImageData(new Uint8ClampedArray(this.originalImageData.data), this.originalImageData.width);
-		let data = imageData.data;
-		let brightnessValue = parseInt(this.changes.brightness) || 0;
-		let contrastValue = parseInt(this.changes.contrast) || 0;
-		let saturationValue = parseInt(this.changes.saturation) || 0;
-		this.ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-		for(let i=0; i<data.length; i+=4) {
-			// brightness
-			data[i] += brightnessValue;
-			data[i+1] += brightnessValue;
-			data[i+2] += brightnessValue;
-
-			// contrast
-			let avg = (data[i]+data[i+1]+data[i+2])/3;
-			if(avg > 122.5) {
-				// bright pixel
-				// equaly increase else color will change
-				// bright to more bright, further for two bright pixels increase in brightness of less bright pixel should be less than that of more bright pixel, so it can create contrast even in between two bright pixels
-				// to do so the value increased or decreased should depends on brightness of pixel
-				let value = contrastValue*((avg-122.5)/100)
-				data[i] += value;
-				data[i+1] += value;
-				data[i+2] += value;
-			}
-			else {
-				// dark pixel
-				// dark to more dark, less the avg value =  more dark 
-				let value = contrastValue*((122.5-avg)/100);
-				data[i] -= value;
-				data[i+1] -= value;
-				data[i+2] -= value;
-			}
-
-			// saturation (increase the value of color which is more lol)
-			// at -100 saturation image should be b/w i.e. rgb = avg,
-		 	// if saturation is decreasing the gap between r, g, b values and avg should be decreasing, if saturation is -50 gap now should be half as it was before
-		 	// if saturation is increasing the gap betweem r, g, b values and avg should be increasing
-		 	// using saturation as percentage of diff increased or decreased as compared to original difference 
-			avg = (data[i]+data[i+1]+data[i+2])/3;
-			data[i] += (((data[i]-avg)/100)*saturationValue);
-			data[i+1] += (((data[i+1]-avg)/100)*saturationValue);
-			data[i+2] += (((data[i+2]-avg)/100)*saturationValue);
-
-		}
-
-		this.ctx.putImageData(imageData, 0, 0);
 	}
 
 	changeCanvasDimensions = (imageHeight, imageWidth) => {
@@ -358,7 +404,8 @@ class Main {
 		this.ctx = canvas.getContext('2d');
 		this.ctx.drawImage(this.editedImage, 0, 0, canvas.width, canvas.height);
 		let imageData = this.ctx.getImageData(0, 0, canvas.width, canvas.height);
-		this.originalImageData = new ImageData(new Uint8ClampedArray(imageData.data), imageData.width);
+		this.imageData = new ImageData(new Uint8ClampedArray(imageData.data), imageData.width);
+		this.rotateObject.initializeCtx(this.ctx);
 	}
 
 	handleFileReader = (e) => {
