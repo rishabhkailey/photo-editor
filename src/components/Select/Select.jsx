c2bae46a3cd44b223e274d5f281435f92752e436
rishabhkailey <rishabhveersinghkailay@gmail.com>
2020-11-15 14:59:14 +0530

    added circle selection option

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -79,7 +79,7 @@ class Select extends Component {
             for (let j = 0; j < this.selectionMapping[i].length; j++) {
 
                 let isInside = false;
-                if (i >= smallY && i <= largeY && j > smallX && j <= largeX) {
+                if (i >= smallY && i <= largeY && j >= smallX && j <= largeX) {
                     isInside = true;
                 }
 
@@ -101,20 +101,53 @@ class Select extends Component {
         if (!this.props.isImageLoaded)
             return;
 
-        if (this.lastShape && this.lastShape.shape === "rectangle") {
-            console.log("updating select mapping rectangle")
-            let { smallX, smallY, largeX, largeY } = this.lastShape.coordinates;
+        if (this.lastShape) {
+            switch (this.lastShape.shape) {
+                case "rectangle":
+                    {
+                        console.log("updating select mapping rectangle")
+                        let { smallX, smallY, largeX, largeY } = this.lastShape.coordinates;
 
-            for (let i = smallX; i < largeX; i++) {
-                for (let j = smallY; j < largeY; j++) {
-                    this.selectionMapping[j][i] = this.state.selectMode === "+";
-                }
+                        for (let i = smallX; i <= largeX; i++) {
+                            for (let j = smallY; j <= largeY; j++) {
+                                this.selectionMapping[j][i] = this.state.selectMode === "+";
+                            }
+                        }
+                        break;
+                    }
+                case "circle":
+                    {
+                        const isInsideCircle = (x, y, x1, y1, x2, y2) => {
+
+                            // center 
+                            let a = (x1 + x2) / 2;
+                            let b = (y1 + y2) / 2;
+
+                            let radiusSquare = Math.pow(x1 - a, 2) + Math.pow(y1 - b, 2);
+
+                            return Math.pow(x - a, 2) + Math.pow(y - b, 2) <= radiusSquare;
+
+                        }
+
+                        let { smallX: x1, smallY: y1, largeX: x2, largeY: y2 } = this.lastShape.coordinates;
+
+                        for (let i = 0; i < this.selectionMapping.length; i++) {
+                            for (let j = 0; j < this.selectionMapping[i].length; j++) {
+
+                                if (isInsideCircle(i, j, x1, y1, x2, y2)) {
+                                    this.selectionMapping[j][i] = this.state.selectMode === "+";
+                                }
+                            }
+                        }
+                        break;
+                    }
             }
 
         }
         this.selectionEnabled = true;
         this.props.setSelectionInfo({ selectionEnabled: true, selectionMapping: this.selectionMapping, fullResSelectionMapping: null })
     }
+
 
     static getDerivedStateFromProps(props) {
         if (props.isImageLoaded) {
@@ -137,6 +170,108 @@ class Select extends Component {
         let canvas = canvasElements.canvas.current;
 
         this.drawShapeFunction = this.drawRectangle;
+
+        canvas.onmousedown = this.startTracking;
+    }
+
+    drawCircle = (e) => {
+
+        let position = { x: e.clientX, y: e.clientY };
+        let image = this.props.canvasFunctions.getEditedImage();
+        let data = image.data;
+        console.log(this.props);
+        let boundingRect = this.props.canvasElements.canvas.current.getBoundingClientRect();
+
+        if (position.x < boundingRect.left) {
+            position.x = boundingRect.left;
+        }
+        else if (position.x > boundingRect.right) {
+            position.x = boundingRect.right;
+        }
+
+        if (position.y < boundingRect.top) {
+            position.y = boundingRect.top;
+        }
+        else if (position.y > boundingRect.bottom) {
+            position.y = boundingRect.bottom;
+        }
+
+        // start position is set by onMouseDown event
+        // x1, y1, x2, y2 are relative postions to the canvas
+        let x1 = this.startPosition.x - boundingRect.left;
+        let y1 = this.startPosition.y - boundingRect.top;
+
+        let x2 = position.x - boundingRect.left;
+        let y2 = position.y - boundingRect.top;
+
+        // console.log(x1, y1);
+        // console.log(x2, y2);
+
+        let smallX, largeX, largeY, smallY;
+        if (x1 < x2) {
+            smallX = parseInt(x1);
+            largeX = parseInt(x2);
+        }
+        else {
+            smallX = parseInt(x2);
+            largeX = parseInt(x1);
+        }
+
+        if (y1 < y2) {
+            largeY = parseInt(y2);
+            smallY = parseInt(y1);
+        }
+        else {
+            largeY = parseInt(y1);
+            smallY = parseInt(y2);
+        }
+
+        // console.log(smallX, smallY, largeX, largeY);
+
+        // console.log(this.selectionMapping.length, this.selectionMapping[0].length);
+
+        // console.log(this.state.selectMode === "+");
+
+        const isInsideCircle = (x, y, x1, y1, x2, y2) => {
+
+            // center 
+            let a = (x1 + x2) / 2;
+            let b = (y1 + y2) / 2;
+
+            let radiusSquare = Math.pow(x1 - a, 2) + Math.pow(y1 - b, 2);
+
+            return Math.pow(x - a, 2) + Math.pow(y - b, 2) <= radiusSquare;
+
+        }
+
+        for (let i = 0; i < this.selectionMapping.length; i++) {
+            for (let j = 0; j < this.selectionMapping[i].length; j++) {
+
+                let isInside = false;
+                if (isInsideCircle(j, i, x1, y1, x2, y2)) {
+                    isInside = true;
+                }
+
+                if (isInside) {
+                    data[(i * image.width + j) * 4 + 3] = this.state.selectMode === "+" ? 255 : 127;
+                }
+                else {
+                    data[(i * image.width + j) * 4 + 3] = this.selectionMapping[i][j] ? 255 : 127;
+                }
+            }
+        }
+        // console.log(image, image.width);
+        this.props.canvasFunctions.setDisplayImage(image);
+        this.lastShape = { shape: "circle", coordinates: { smallX, smallY, largeX, largeY } }
+        this.prevPosition = position;
+    }
+
+    circleSelection = () => {
+        let { canvasFunctions, canvasElements } = this.props;
+        console.log(this.props, canvasElements);
+        let canvas = canvasElements.canvas.current;
+
+        this.drawShapeFunction = this.drawCircle;
 
         canvas.onmousedown = this.startTracking;
     }
@@ -265,8 +400,10 @@ class Select extends Component {
             <hr />
 
             <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                <h5>select shape</h5>
+                <h5>select method</h5>
                 <button className="btn btn-primary" onClick={this.rectangleSelection}>Rectangle</button>
+                <hr />
+                <button className="btn btn-primary" onClick={this.circleSelection}>circle</button>
             </div>
 
         </div>
